499dfefc13d176a05b9f0cf741f3f57b93cc14c0
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-10-28 19:45:35 -0500

    first commit

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,15 @@
+/*
+function HelloWorldApp(){
+    // document.createElement
+    return (<h1>Hola Mundo!!!</h1>);
+}
+
+export default HelloWorldApp;
+*/
+import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hola Mundo!!!!!</h1>
+  )
+}
