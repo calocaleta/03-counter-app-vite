55ada83be45f86aa4bd749d78c5bbb24ea6ab7f2
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-10-28 20:19:46 -0500

    Prueba de componente simple

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+const newMessage = {
+    message: 'Hola Mundo',
+    title: 'Carlos'
+};
+
+const fExp = (valor) => {
+    return valor*valor;
+}
+
 const FirstApp = () => {
+    //const newMessage = 'Nuevo Mensaje!';
+    
   return (
-    <h1>FirstApp</h1>
+    <>
+        <h1>{ fExp(3) }</h1>
+        <code>{JSON.stringify(newMessage)}</code>
+        {/* <code>{JSON.stringify(newMessage)}</code> */}
+        <p>Subtitulo</p>
+    </>
   )
 }
 
