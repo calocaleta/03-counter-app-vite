8388a7f5c085c3ec9ef37feb0e35af52cbb16a6b
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-11-01 23:03:45 -0500

    Implementando UseState

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,18 +1,23 @@
+import {useState} from 'react';
 import React from 'react';
 import PropTypes from 'prop-types'
 
-const handleAdd = (event) => {
-    console.log(event);
-    value = 1000;
-}
-
-
 const CounterApp = ({value}) => {
+
+    const [ counter, setCounter] = useState(value);  
+
+    const handleAdd = (event) => {
+        //console.log(event);
+        //value = 1000;
+        //setCounter(counter + 1);
+        setCounter((prevValue) => prevValue + 2);
+    }
+
     
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
             <button onClick={ handleAdd }>
                 +1
             </button>
