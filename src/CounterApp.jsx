52302073e009b770af6238b91c6748029fceb82d
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-11-01 23:16:36 -0500

    Implementando funciones de resta y reseteo

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -4,23 +4,30 @@ import PropTypes from 'prop-types'
 
 const CounterApp = ({value}) => {
 
+    console.log('render');
+
     const [ counter, setCounter] = useState(value);  
 
     const handleAdd = (event) => {
         //console.log(event);
         //value = 1000;
         //setCounter(counter + 1);
-        setCounter((prevValue) => prevValue + 2);
+        setCounter((prevValue) => prevValue + 1);
     }
 
-    
+    const handleSubstract = () => setCounter(counter - 1);
+
+    const handleReset = () => {
+        setCounter(value);
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2> { counter } </h2>
-            <button onClick={ handleAdd }>
-                +1
-            </button>
+            <button onClick={ handleAdd }> +1 </button>
+            <button onClick={ handleSubstract }> -1 </button>
+            <button onClick={ handleReset }> Reset </button>
         </>
     );
 };
