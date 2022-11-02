02e4b5b764334aeadec537b4bb3445158b16b304
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-11-01 22:49:54 -0500

    Agregando evento Click

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
+const handleAdd = (event) => {
+    console.log(event);
+    value = 1000;
+}
+
+
 const CounterApp = ({value}) => {
+    
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{ value }</h2>
+            <h2> { value } </h2>
+            <button onClick={ handleAdd }>
+                +1
+            </button>
         </>
     );
 };
