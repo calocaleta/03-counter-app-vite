23de1464a0abb2b368925818fc0453a2c710834d
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-11-20 20:10:54 -0500

    uso de toBe con Jest

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,5 +1,14 @@
-test('Esta prueba no debe de fallar',() => {
-    if(0 === 0){
-        throw new Error('No puede dividir entre 0');
-    }
+describe('Pruebas en <DemoComponent/>', () => {
+
+    test('Esta prueba no debe de fallar',() => {
+        // 1. Inicialización
+        const message1 = 'Hola Mundo';
+
+        // 2. Estimulo
+        const message2 = message1.trim();
+
+        // 3. Observar el comportamiento... esperado
+        expect(message1).toBe(message2);
+        
+    })
 })
