fdc06d48b717c9001846d8c1b340bdd5c5ac193d
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-11-22 22:53:47 -0500

    Prueba toEqual

--- test/base-pruebas/02-template-string.test.js
+++ test/base-pruebas/02-template-string.test.js
@@ -7,7 +7,7 @@ describe('Pruebas en 02-template-string', () => {
         const name = 'Fernando';
         const message = getSaludo(name);
 
-        expect(message).toBe(`Hola ${name}!!`);
+        expect(message).toBe(`Hola ${name}`);
     });
 
  })
