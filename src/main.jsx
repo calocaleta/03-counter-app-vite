a7de3b8d6f1adbf4287c6648f1615a8531de6e1f
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-11-29 23:36:57 -0500

    Preparando testeo de componentes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CounterApp from './CounterApp';
+//import CounterApp from './CounterApp';
 import FirstApp from './FirstApp';
-import {HelloWorldApp} from './HelloWorldApp'
+//import {HelloWorldApp} from './HelloWorldApp'
 
 
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HelloWorldApp />
+        {/*<HelloWorldApp />*/}
         <FirstApp title="Hola ejemploo" subtitle={123} />
-        <CounterApp value={1} />
+        {/*<CounterApp value={1} />*/}
     </React.StrictMode>
 );
