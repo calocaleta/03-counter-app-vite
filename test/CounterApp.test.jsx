c43bcca3b18668deb2d2448ab6d10dcbcdb1d16e
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-12-04 13:48:31 -0500

    Probando función fireEvent

--- test/CounterApp.test.jsx
+++ test/CounterApp.test.jsx
@@ -1,11 +1,12 @@
 /** @jest-environment jsdom */
-import { render,screen } from "@testing-library/react";
+import { fireEvent, render,screen } from "@testing-library/react";
 import CounterApp from "../src/CounterApp";
 
 describe('Pruebas con el componente CounterApp', () => { 
-    
+    const initialValue = 1;
+
     test('Prueba hacer match con snapshot',() => {
-        const {container} = render( <CounterApp value={1} /> );
+        const {container} = render( <CounterApp value={initialValue} /> );
         expect(container).toMatchSnapshot();
     });
 
@@ -14,5 +15,32 @@ describe('Pruebas con el componente CounterApp', () => {
         //expect(screen.getByText(100)).toBeTruthy();
         expect(screen.getByRole('heading',{level:2}).innerHTML).toContain('100');
     });
+
+    test('Debe de incrementar con el botón +1', () => {
+
+        render(<CounterApp value={initialValue} />);
+        fireEvent.click( screen.getByText('+1') );
+        expect(screen.getByText('2')).toBeTruthy();
+    });
+
+    test('Debe de decrementar con el botón -1', () => {
+
+        render(<CounterApp value={initialValue} />);
+        fireEvent.click( screen.getByText('-1') );
+        //screen.debug();
+        expect(screen.getByText('0')).toBeTruthy();
+    });
+
+    test('Debe funcionar el botón de reset', () => {
+        render(<CounterApp value={initialValue} />);
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        //fireEvent.click( screen.getByText('Reset') );
+        fireEvent.click(screen.getByRole('button',{name: 'btn-reset'}));
+
+        expect(screen.getByText(initialValue)).toBeTruthy();
+    })
+
 
  })
